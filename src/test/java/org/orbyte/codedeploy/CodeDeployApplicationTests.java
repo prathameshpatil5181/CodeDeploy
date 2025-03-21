9b934c5fd0a96f9dfb055f70a462c0292ce8e78b
abhi patil <abhipatil@abhis-MacBook-Air.local>
2025-03-21 22:29:54 +0530

    first commit

---
+++ src/test/java/org/orbyte/codedeploy/CodeDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orbyte.codedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
